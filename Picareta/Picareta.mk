1c0e3db8b8230b53fb93412b61f48e6543de8b8c
pvsfair <pvsfair@gmail.com>
2014-10-06 14:24:17 -0300

    asd

--- Picareta/Picareta.mk
+++ Picareta/Picareta.mk
@@ -5,16 +5,16 @@
 ## Debug
 ProjectName            :=Picareta
 ConfigurationName      :=Debug
-WorkspacePath          := "C:\Users\Paulo\Documents\GitHub\Picareta"
-ProjectPath            := "C:\Users\Paulo\Documents\GitHub\Picareta\Picareta"
+WorkspacePath          := "C:\NovaPasta\ProgII"
+ProjectPath            := "C:\Users\LAB4-PC10\Documents\GitHub\Picareta\Picareta"
 IntermediateDirectory  :=./Debug
 OutDir                 := $(IntermediateDirectory)
 CurrentFileName        :=
 CurrentFilePath        :=
 CurrentFileFullPath    :=
-User                   :=Paulo
-Date                   :=10/05/14
-CodeLitePath           :="C:\Program Files (x86)\CodeLite"
+User                   :=LAB4-PC10
+Date                   :=06/10/2014
+CodeLitePath           :="C:\Program Files\CodeLite"
 LinkerName             :=g++
 SharedObjectLinkerName :=g++ -shared -fPIC
 ObjectSuffix           :=.o
@@ -38,32 +38,30 @@ MakeDirCommand         :=makedir
 RcCmpOptions           := 
 RcCompilerName         :=windres
 LinkOptions            :=  -pg 
-IncludePath            := $(IncludeSwitch)"C:/Program Files (x86)/Graphviz2.38/include/graphviz"  $(IncludeSwitch). $(IncludeSwitch). 
+IncludePath            :=  $(IncludeSwitch). $(IncludeSwitch). 
 IncludePCH             := 
 RcIncludePath          := 
 Libs                   := 
 ArLibs                 :=  
-LibPath                :=$(LibraryPathSwitch)"C:/Program Files (x86)/Graphviz2.38/lib/release/lib"  $(LibraryPathSwitch). 
+LibPath                := $(LibraryPathSwitch). 
 
 ##
 ## Common variables
-## AR, CXX, CC, AS, CXXFLAGS and CFLAGS can be overriden using an environment variables
+## AR, CXX, CC, CXXFLAGS and CFLAGS can be overriden using an environment variables
 ##
 AR       := ar rcus
 CXX      := g++
 CC       := gcc
 CXXFLAGS :=  -g -pg -O0 -Wall $(Preprocessors)
 CFLAGS   :=  -g -pg -O0 -Wall $(Preprocessors)
-ASFLAGS  := 
-AS       := as
 
 
 ##
 ## User defined environment variables
 ##
-CodeLiteDir:=C:\Program Files (x86)\CodeLite
+CodeLiteDir:=C:\Program Files\CodeLite
 UNIT_TEST_PP_SRC_DIR:=C:\UnitTest++-1.3
-Objects0=$(IntermediateDirectory)/main.cpp$(ObjectSuffix) $(IntermediateDirectory)/Picareta.cpp$(ObjectSuffix) $(IntermediateDirectory)/Ferramenta.cpp$(ObjectSuffix) $(IntermediateDirectory)/Inventario.cpp$(ObjectSuffix) $(IntermediateDirectory)/Bloco.cpp$(ObjectSuffix) 
+Objects0=$(IntermediateDirectory)/main$(ObjectSuffix) $(IntermediateDirectory)/Picareta$(ObjectSuffix) $(IntermediateDirectory)/Ferramenta$(ObjectSuffix) $(IntermediateDirectory)/Inventario$(ObjectSuffix) $(IntermediateDirectory)/Bloco$(ObjectSuffix) 
 
 
 
@@ -90,45 +88,45 @@ PreBuild:
 ##
 ## Objects
 ##
-$(IntermediateDirectory)/main.cpp$(ObjectSuffix): main.cpp $(IntermediateDirectory)/main.cpp$(DependSuffix)
-	$(CXX) $(IncludePCH) $(SourceSwitch) "C:/Users/Paulo/Documents/GitHub/Picareta/Picareta/main.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/main.cpp$(ObjectSuffix) $(IncludePath)
-$(IntermediateDirectory)/main.cpp$(DependSuffix): main.cpp
-	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/main.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/main.cpp$(DependSuffix) -MM "main.cpp"
+$(IntermediateDirectory)/main$(ObjectSuffix): main.cpp $(IntermediateDirectory)/main$(DependSuffix)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "C:/Users/LAB4-PC10/Documents/GitHub/Picareta/Picareta/main.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/main$(ObjectSuffix) $(IncludePath)
+$(IntermediateDirectory)/main$(DependSuffix): main.cpp
+	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/main$(ObjectSuffix) -MF$(IntermediateDirectory)/main$(DependSuffix) -MM "main.cpp"
 
-$(IntermediateDirectory)/main.cpp$(PreprocessSuffix): main.cpp
-	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/main.cpp$(PreprocessSuffix) "main.cpp"
+$(IntermediateDirectory)/main$(PreprocessSuffix): main.cpp
+	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/main$(PreprocessSuffix) "main.cpp"
 
-$(IntermediateDirectory)/Picareta.cpp$(ObjectSuffix): Picareta.cpp $(IntermediateDirectory)/Picareta.cpp$(DependSuffix)
-	$(CXX) $(IncludePCH) $(SourceSwitch) "C:/Users/Paulo/Documents/GitHub/Picareta/Picareta/Picareta.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/Picareta.cpp$(ObjectSuffix) $(IncludePath)
-$(IntermediateDirectory)/Picareta.cpp$(DependSuffix): Picareta.cpp
-	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/Picareta.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/Picareta.cpp$(DependSuffix) -MM "Picareta.cpp"
+$(IntermediateDirectory)/Picareta$(ObjectSuffix): Picareta.cpp $(IntermediateDirectory)/Picareta$(DependSuffix)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "C:/Users/LAB4-PC10/Documents/GitHub/Picareta/Picareta/Picareta.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/Picareta$(ObjectSuffix) $(IncludePath)
+$(IntermediateDirectory)/Picareta$(DependSuffix): Picareta.cpp
+	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/Picareta$(ObjectSuffix) -MF$(IntermediateDirectory)/Picareta$(DependSuffix) -MM "Picareta.cpp"
 
-$(IntermediateDirectory)/Picareta.cpp$(PreprocessSuffix): Picareta.cpp
-	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/Picareta.cpp$(PreprocessSuffix) "Picareta.cpp"
+$(IntermediateDirectory)/Picareta$(PreprocessSuffix): Picareta.cpp
+	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/Picareta$(PreprocessSuffix) "Picareta.cpp"
 
-$(IntermediateDirectory)/Ferramenta.cpp$(ObjectSuffix): Ferramenta.cpp $(IntermediateDirectory)/Ferramenta.cpp$(DependSuffix)
-	$(CXX) $(IncludePCH) $(SourceSwitch) "C:/Users/Paulo/Documents/GitHub/Picareta/Picareta/Ferramenta.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/Ferramenta.cpp$(ObjectSuffix) $(IncludePath)
-$(IntermediateDirectory)/Ferramenta.cpp$(DependSuffix): Ferramenta.cpp
-	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/Ferramenta.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/Ferramenta.cpp$(DependSuffix) -MM "Ferramenta.cpp"
+$(IntermediateDirectory)/Ferramenta$(ObjectSuffix): Ferramenta.cpp $(IntermediateDirectory)/Ferramenta$(DependSuffix)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "C:/Users/LAB4-PC10/Documents/GitHub/Picareta/Picareta/Ferramenta.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/Ferramenta$(ObjectSuffix) $(IncludePath)
+$(IntermediateDirectory)/Ferramenta$(DependSuffix): Ferramenta.cpp
+	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/Ferramenta$(ObjectSuffix) -MF$(IntermediateDirectory)/Ferramenta$(DependSuffix) -MM "Ferramenta.cpp"
 
-$(IntermediateDirectory)/Ferramenta.cpp$(PreprocessSuffix): Ferramenta.cpp
-	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/Ferramenta.cpp$(PreprocessSuffix) "Ferramenta.cpp"
+$(IntermediateDirectory)/Ferramenta$(PreprocessSuffix): Ferramenta.cpp
+	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/Ferramenta$(PreprocessSuffix) "Ferramenta.cpp"
 
-$(IntermediateDirectory)/Inventario.cpp$(ObjectSuffix): Inventario.cpp $(IntermediateDirectory)/Inventario.cpp$(DependSuffix)
-	$(CXX) $(IncludePCH) $(SourceSwitch) "C:/Users/Paulo/Documents/GitHub/Picareta/Picareta/Inventario.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/Inventario.cpp$(ObjectSuffix) $(IncludePath)
-$(IntermediateDirectory)/Inventario.cpp$(DependSuffix): Inventario.cpp
-	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/Inventario.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/Inventario.cpp$(DependSuffix) -MM "Inventario.cpp"
+$(IntermediateDirectory)/Inventario$(ObjectSuffix): Inventario.cpp $(IntermediateDirectory)/Inventario$(DependSuffix)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "C:/Users/LAB4-PC10/Documents/GitHub/Picareta/Picareta/Inventario.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/Inventario$(ObjectSuffix) $(IncludePath)
+$(IntermediateDirectory)/Inventario$(DependSuffix): Inventario.cpp
+	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/Inventario$(ObjectSuffix) -MF$(IntermediateDirectory)/Inventario$(DependSuffix) -MM "Inventario.cpp"
 
-$(IntermediateDirectory)/Inventario.cpp$(PreprocessSuffix): Inventario.cpp
-	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/Inventario.cpp$(PreprocessSuffix) "Inventario.cpp"
+$(IntermediateDirectory)/Inventario$(PreprocessSuffix): Inventario.cpp
+	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/Inventario$(PreprocessSuffix) "Inventario.cpp"
 
-$(IntermediateDirectory)/Bloco.cpp$(ObjectSuffix): Bloco.cpp $(IntermediateDirectory)/Bloco.cpp$(DependSuffix)
-	$(CXX) $(IncludePCH) $(SourceSwitch) "C:/Users/Paulo/Documents/GitHub/Picareta/Picareta/Bloco.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/Bloco.cpp$(ObjectSuffix) $(IncludePath)
-$(IntermediateDirectory)/Bloco.cpp$(DependSuffix): Bloco.cpp
-	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/Bloco.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/Bloco.cpp$(DependSuffix) -MM "Bloco.cpp"
+$(IntermediateDirectory)/Bloco$(ObjectSuffix): Bloco.cpp $(IntermediateDirectory)/Bloco$(DependSuffix)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "C:/Users/LAB4-PC10/Documents/GitHub/Picareta/Picareta/Bloco.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/Bloco$(ObjectSuffix) $(IncludePath)
+$(IntermediateDirectory)/Bloco$(DependSuffix): Bloco.cpp
+	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/Bloco$(ObjectSuffix) -MF$(IntermediateDirectory)/Bloco$(DependSuffix) -MM "Bloco.cpp"
 
-$(IntermediateDirectory)/Bloco.cpp$(PreprocessSuffix): Bloco.cpp
-	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/Bloco.cpp$(PreprocessSuffix) "Bloco.cpp"
+$(IntermediateDirectory)/Bloco$(PreprocessSuffix): Bloco.cpp
+	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/Bloco$(PreprocessSuffix) "Bloco.cpp"
 
 
 -include $(IntermediateDirectory)/*$(DependSuffix)
@@ -136,10 +134,23 @@ $(IntermediateDirectory)/Bloco.cpp$(PreprocessSuffix): Bloco.cpp
 ## Clean
 ##
 clean:
-	$(RM) ./Debug/*$(ObjectSuffix)
-	$(RM) ./Debug/*$(DependSuffix)
+	$(RM) $(IntermediateDirectory)/main$(ObjectSuffix)
+	$(RM) $(IntermediateDirectory)/main$(DependSuffix)
+	$(RM) $(IntermediateDirectory)/main$(PreprocessSuffix)
+	$(RM) $(IntermediateDirectory)/Picareta$(ObjectSuffix)
+	$(RM) $(IntermediateDirectory)/Picareta$(DependSuffix)
+	$(RM) $(IntermediateDirectory)/Picareta$(PreprocessSuffix)
+	$(RM) $(IntermediateDirectory)/Ferramenta$(ObjectSuffix)
+	$(RM) $(IntermediateDirectory)/Ferramenta$(DependSuffix)
+	$(RM) $(IntermediateDirectory)/Ferramenta$(PreprocessSuffix)
+	$(RM) $(IntermediateDirectory)/Inventario$(ObjectSuffix)
+	$(RM) $(IntermediateDirectory)/Inventario$(DependSuffix)
+	$(RM) $(IntermediateDirectory)/Inventario$(PreprocessSuffix)
+	$(RM) $(IntermediateDirectory)/Bloco$(ObjectSuffix)
+	$(RM) $(IntermediateDirectory)/Bloco$(DependSuffix)
+	$(RM) $(IntermediateDirectory)/Bloco$(PreprocessSuffix)
 	$(RM) $(OutputFile)
 	$(RM) $(OutputFile).exe
-	$(RM) "../.build-debug/Picareta"
+	$(RM) "../../../../../../NovaPasta/ProgII/.build-debug/Picareta"
 
 
